c509f045ea7bee9de40cdd53661ec3a354ac690f
Kristian <kglomset@gmail.com>
2024-03-19 12:33:06 +0100

    Started on setting up structs

--- resources/structs.go
+++ resources/structs.go
@@ -1,1 +1,16 @@
 package resources
+
+type Coordinates struct {
+	Latitude  float64 `json:"latitude"`
+	Longitude float64 `json:"longitude"`
+}
+
+type Features struct {
+	Temperature      float64     `json:"temperature"`
+	Precipitation    bool        `json:"precipitation"`
+	Capital          bool        `json:"capital"`
+	Coordinates      Coordinates `json:"coordinates"`
+	Population       bool        `json:"population"`
+	Area             bool        `json:"area"`
+	TargetCurrencies []string    `json:"targetCurrencies"`
+}
