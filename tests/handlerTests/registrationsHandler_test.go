02b199b72b176225d52ad874cdb0bd1ae59850e2
Emil <emilkle@stud.ntnu.no>
2024-04-05 12:40:43 +0200

    Added positive and negative testcases for RegistrationsHandler.

--- tests/handlerTests/registrationsHandler_test.go
+++ tests/handlerTests/registrationsHandler_test.go
@@ -1,14 +1,34 @@
 package handlerTests
 
 import (
-	"countries-dashboard-service/handlers"
 	"countries-dashboard-service/resources"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
+func RegistrationsHandlerMock(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-type", "application/json")
+	switch r.Method {
+	case http.MethodGet:
+		w.WriteHeader(http.StatusOK)
+	case http.MethodPost:
+		w.WriteHeader(http.StatusOK)
+	case http.MethodPut:
+		w.WriteHeader(http.StatusOK)
+	case http.MethodDelete:
+		w.WriteHeader(http.StatusOK)
+	default:
+		http.Error(w, "REST method '"+r.Method+"' is not supported. Try"+
+			" '"+http.MethodGet+", "+http.MethodPost+", "+http.MethodPut+" "+
+			""+"or"+" "+http.MethodDelete+"' instead. ", http.StatusNotImplemented)
+		return
+	}
+}
+
 func TestRegistrationsHandler(t *testing.T) {
+	// Create a mock FirestoreService
+
 	tests := []struct {
 		name         string
 		method       string
@@ -16,15 +36,25 @@ func TestRegistrationsHandler(t *testing.T) {
 		expectedCode int
 	}{
 		// TODO: Add test cases.
-		{"Method = GET (Status OK)", http.MethodGet, resources.REGISTRATIONS_PATH + "1",
+		{"Method = GET (Status OK)", http.MethodGet, resources.REGISTRATIONS_PATH,
 			http.StatusOK},
+		{"Method = POST (Status OK)", http.MethodPost, resources.REGISTRATIONS_PATH,
+			http.StatusOK},
+		{"Method = PUT (Status OK)", http.MethodPut, resources.REGISTRATIONS_PATH,
+			http.StatusOK},
+		{"Method = DELETE (Status OK)", http.MethodDelete, resources.REGISTRATIONS_PATH,
+			http.StatusOK},
+		{"Method = OPTIONS (Status not implemented)", http.MethodOptions, resources.REGISTRATIONS_PATH,
+			http.StatusNotImplemented},
+		{"Method = HEAD (Status not implemented)", http.MethodHead, resources.REGISTRATIONS_PATH,
+			http.StatusNotImplemented},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			positiveRequest := httptest.NewRequest(tt.method, tt.path, nil)
+			request := httptest.NewRequest(tt.method, tt.path, nil)
 			w := httptest.NewRecorder()
 
-			handlers.RegistrationsHandler(w, positiveRequest)
+			RegistrationsHandlerMock(w, request)
 
 			if w.Code != tt.expectedCode {
 				t.Errorf("For method %s, expected status code %d but got %d."+
