536acf4e03f2ae064c602a4ede178b8db10cb61b
Marius R. Dahl <mariusrd@stud.ntnu.no>
2024-04-13 10:33:31 +0200

    Accidently deleted changes done to the CheckEndpointStatus function and the test file for the same function. This merge is readding the code that was deleted.

---
+++ tests/functionTests/statusFunctions_test.go
@@ -0,0 +1,61 @@
+package functionTests
+
+import (
+	"countries-dashboard-service/functions"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestCheckEndpointStatus(t *testing.T) {
+	// Struct for test cases
+	tests := []struct {
+		name               string // Name of the test
+		mockResponseStatus int    // A mocked status response returned by the test server
+		want               int    // The status that is wanted to be returned in the test
+		shutdownServer     bool   // Determine whether to simulate a network error by shutting down the server
+	}{
+		{
+			name:               "StatusOK",
+			mockResponseStatus: http.StatusOK,
+			want:               http.StatusOK,
+		},
+		{
+			name:               "StatusInternalServerError",
+			mockResponseStatus: http.StatusInternalServerError,
+			want:               http.StatusInternalServerError,
+		},
+		{
+			name:               "NetworkError",
+			mockResponseStatus: http.StatusOK, // This value will be ignored because the server will be shut down
+			want:               http.StatusServiceUnavailable,
+			shutdownServer:     true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// Creates a mock server for testing
+			server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.WriteHeader(tt.mockResponseStatus)
+			}))
+
+			// Shut down server to simulate network error
+			if tt.shutdownServer {
+				server.Close()
+			}
+
+			// Call the CheckEndpointStatus function
+			got := functions.CheckEndpointStatus(server.URL)
+
+			// Check if the retrieved status is different from the wanted response
+			if got != tt.want {
+				t.Errorf("%s: CheckEndpointStatus() = %v, want %v", tt.name, got, tt.want)
+			}
+
+			if !tt.shutdownServer {
+				server.Close()
+			}
+		})
+	}
+}
